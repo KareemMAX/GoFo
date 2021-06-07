ccdcc24e4d662ba8b1df607836c1cba7f9863208
Jonathan Ghaly <60841107+JonathanGhaly@users.noreply.github.com>
2021-06-07 03:53:40 +0200

    Update Booking.java

--- src/player/Booking.java
+++ src/player/Booking.java
@@ -1,4 +1,36 @@
 package player;
 
+import playgroundOwner.Playground;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/** Player Class for GoFo App
+ *
+ * @author Jonathan Saad
+ * @version 1.00 2021/6/6
+ * Course: Software Engineering 1 CS251 2020/2021 - Homework 4 Final Draft
+ */
 public class Booking {
+    public Playground playground;
+    public Team players;
+    public LocalDateTime date;
+    public float time;
+    public Player player;
+    public int id;
+    public float totalCost;
+
+    public Booking(Playground playground, Team players, LocalDateTime date,float time, Player player, int id) {
+        this.playground = playground;
+        this.players = players;
+        this.date = date;
+        this.player = player;
+        this.id = id;
+        this.time = time;
+        this.totalCost = getMoney (time);
+    }
+    public float getMoney(float time){
+        return time * playground.pricePerHour;
+    }
 }
